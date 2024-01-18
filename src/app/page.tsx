e41f0d08996246405e8ec2ab5da3d95b698ff66a
jalal-ahmed88 <jamnazeerahmed9@gmail.com>
2024-01-18 12:35:48 -0800

    update

---
+++ src/app/page.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <div>
+  <div className="m-3 p-6 bg-blue-100 rounded-lg">
+    <p>“The success combination in business is: Do what you do better... and: do more of what you do.”</p>
+    <span>- David J. Schwartz</span>
+  </div>
+  <div className="m-3 p-6 bg-blue-100 rounded-lg">
+    <p>“Give out what you most want to come back.”</p>
+    <span>- Robin Sharma</span>
+  </div>
+  <div className="m-3 p-6 bg-blue-100 rounded-lg">
+    <p>“You don't have to be great at something to start, but you have to start to be great at something.”</p>
+    <span>- Zig Ziglar</span>
+    <div className="wrapper">
+  <div className="menu">
+    <a href="#" className="active">Profile</a>
+    <a href="#">Notifications</a>
+    <a href="#">Payments</a>
+    <a href="#">Settings</a>
+  </div>
+</div>
+
+
+  </div>
+</div>
+
+    </div>
+  )
+}
+
+export default page
